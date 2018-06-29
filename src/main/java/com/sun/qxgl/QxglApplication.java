49bb29f649502bef8348424f7f55cd893aee4122
charlidsun <yulinsunjingge@gmail.com>
2018-06-29 10:50:23 +0800

    初始化项目

---
+++ src/main/java/com/sun/qxgl/QxglApplication.java
@@ -0,0 +1,12 @@
+package com.sun.qxgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QxglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QxglApplication.class, args);
+	}
+}
