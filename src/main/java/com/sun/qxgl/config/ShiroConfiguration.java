7364ff506a0ffa03131a6d569168eae906762105
charlidsun <yulinsunjingge@gmail.com>
2018-07-03 18:29:54 +0800

    easyui整合

--- src/main/java/com/sun/qxgl/config/ShiroConfiguration.java
+++ src/main/java/com/sun/qxgl/config/ShiroConfiguration.java
@@ -55,6 +55,8 @@ public class ShiroConfiguration {
 		
 		//<!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
 	    //<!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+		
+		filterChainDefinitionMap.put("/static/*", "anon"); //匿名访问静态资源
 		filterChainDefinitionMap.put("/**", "authc");
 		
 		// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
